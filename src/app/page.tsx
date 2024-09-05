d3cc176dfe4a378daae74d093060886cc1dea3d5
178oi <178megane@gmail.com>
2024-09-06 02:52:57 +0900

    クリックしたらマル反映

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+'use client';
 import { Circle, X } from 'lucide-react';
+import { useState } from 'react';
 
 const Home = () => {
-  const board = [
-    [0, 1, 2],
-    [0, 1, 2],
-    [0, 1, 2],
-  ];
+  const [board, setBoard] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+
+  const handleClick = (x: number, y: number) => {
+    const cloneBoard = structuredClone(board);
+    cloneBoard[y][x] = 1;
+
+    setBoard(cloneBoard);
+  };
   return (
     <div className='flex h-screen w-full items-center justify-center bg-slate-200'>
       <div className='flex size-96 flex-wrap rounded-lg bg-slate-400 shadow-xl'>
@@ -14,6 +23,7 @@ const Home = () => {
             <div
               key={`${x}-${y}`}
               className='flex size-32 items-center justify-center border-slate-300/30 ring-1'
+              onClick={() => handleClick(x, y)}
             >
               {/* <Circle size={120} color='red' />
               <X size={120} color='blue' /> */}
